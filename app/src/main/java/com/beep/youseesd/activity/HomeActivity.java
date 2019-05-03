8b72c4842ef234390eb6879f3a9967eba9f7df02
Yong Do <magicdo7@gmail.com>
2019-05-02 20:07:51 -0700

    refactors HomeActivity

--- app/src/main/java/com/beep/youseesd/activity/HomeActivity.java
+++ app/src/main/java/com/beep/youseesd/activity/HomeActivity.java
@@ -3,48 +3,48 @@ package com.beep.youseesd.activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.beep.youseesd.R;
 import com.beep.youseesd.fragment.HomeFragment;
 import com.beep.youseesd.util.WLog;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import handler.AuthHandler;
 
-public class HomeActivity extends BaseActivity {
-
-    private FloatingActionButton mCreateTourButton;
-    private FirebaseAuth mAuth;
+public class HomeActivity extends BaseActivity implements OnCompleteListener<AuthResult> {
 
     private Toolbar mToolbar;
+    private FloatingActionButton mCreateTourButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        setupUI();
+
+        HomeFragment homeFragment = new HomeFragment();
+        updateFragment(homeFragment);
+
         WLog.i("home launched");
+        WLog.i("check user session..");
+        AuthHandler.signinAnonymously(this, this);
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        WLog.i(currentUser != null ? "uid: " + currentUser.getUid() : "currentUser is null");
+    }
 
-        AuthHandler.signinAnonymously(this);
-
-        mAuth = FirebaseAuth.getInstance();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        WLog.i(currentUser != null ? currentUser.getUid() : "currentUser is null");
-
+    private void setupUI() {
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mToolbar.setTitle(R.string.title_home);
         mToolbar.setTitleTextColor(Color.WHITE);
-
-        HomeFragment homeFragment = new HomeFragment();
-
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.add(R.id.mainframe, homeFragment);
-        transaction.commit();
 
         mCreateTourButton = (FloatingActionButton) findViewById(R.id.btn_create_tour);
         mCreateTourButton.setOnClickListener(new View.OnClickListener() {
@@ -54,5 +54,10 @@ public class HomeActivity extends BaseActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onComplete(@NonNull Task<AuthResult> task) {
+        FirebaseUser user = task.getResult().getUser();
     }
 }
--- app/src/main/java/handler/AuthHandler.java
+++ app/src/main/java/handler/AuthHandler.java
@@ -1,36 +1,18 @@
 package handler;
 
 import android.app.Activity;
-import android.support.annotation.NonNull;
 
-import com.beep.youseesd.util.WLog;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class AuthHandler {
 
-    public static void signinAnonymously(Activity act) {
+    public static void signinAnonymously(Activity act, OnCompleteListener<AuthResult> listener) {
         if (FirebaseAuth.getInstance().getCurrentUser() != null) {
             return;
         }
 
-        FirebaseAuth.getInstance().signInAnonymously()
-                .addOnCompleteListener(act, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            WLog.i("signInAnonymously:failure");
-                        }
-
-                        // ...
-                    }
-                });
+        FirebaseAuth.getInstance().signInAnonymously().addOnCompleteListener(act, listener);
     }
 }
