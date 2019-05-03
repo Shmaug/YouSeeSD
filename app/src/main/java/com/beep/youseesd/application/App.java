6d9511319be7d13da19a8aa346d990ca314f9980
Yong Do <magicdo7@gmail.com>
2019-05-02 21:30:03 -0700

    removes a line for debugging

--- app/src/main/java/com/beep/youseesd/application/App.java
+++ app/src/main/java/com/beep/youseesd/application/App.java
@@ -3,7 +3,6 @@ package com.beep.youseesd.application;
 import android.app.Application;
 
 import com.beep.youseesd.util.WLog;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class App extends Application {
 
@@ -13,9 +12,7 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         singleton = this;
-
         WLog.i("Our app just started!");
-        FirebaseAuth.getInstance().signOut();
     }
 
     public static App getInstance() {
