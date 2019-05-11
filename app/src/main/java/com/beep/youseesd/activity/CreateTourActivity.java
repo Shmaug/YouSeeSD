648abed7378ea226f4fe1f4898f7dc4fef1fe7e3
Yong Do <magicdo7@gmail.com>
2019-05-10 22:29:18 -0700

    done for sample screens

--- app/src/main/java/com/beep/youseesd/activity/CreateTourActivity.java
+++ app/src/main/java/com/beep/youseesd/activity/CreateTourActivity.java
@@ -16,6 +16,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.beep.youseesd.R;
 import com.beep.youseesd.adapter.CreateTourLocationAdapter;
 import com.beep.youseesd.model.Location;
+import com.google.android.material.chip.ChipGroup;
 import com.pedromassango.ibackdrop.Backdrop;
 
 import java.util.ArrayList;
@@ -30,6 +31,8 @@ public class CreateTourActivity extends AppCompatActivity {
     private LinearLayoutManager layoutManager;
     private CreateTourLocationAdapter adapter;
     private TextView locationsFrontTitle;
+
+    private ChipGroup collegeGroup, majorGroup;
 
     @Override
     public void onBackPressed() {
@@ -65,6 +68,16 @@ public class CreateTourActivity extends AppCompatActivity {
         locationsFrontTitle.setText(locations.size() + " Locations Added");
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, layoutManager.getOrientation());
         locationList.addItemDecoration(dividerItemDecoration);
+
+        collegeGroup = (ChipGroup) findViewById(R.id.college_group);
+        majorGroup = (ChipGroup) findViewById(R.id.major_group);
+        for (int i = 0; i < majorGroup.getChildCount(); i++) {
+            majorGroup.getChildAt(i).setOnClickListener(v -> backdrop.closeBackdrop());
+        }
+
+        for (int i = 0; i < collegeGroup.getChildCount(); i++) {
+            collegeGroup.getChildAt(i).setOnClickListener(v -> backdrop.closeBackdrop());
+        }
     }
 
     private List<Location> createLocations() {
