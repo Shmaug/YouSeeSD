8ed5e6c66c59ec82ab145772d23bf6da87313f51
Yong Do <magicdo7@gmail.com>
2019-05-28 14:19:31 -0700

    changes the intro logo

--- app/src/main/java/com/beep/youseesd/model/Theme.java
+++ app/src/main/java/com/beep/youseesd/model/Theme.java
@@ -2,19 +2,19 @@ package com.beep.youseesd.model;
 
 // theme = tags = hashtags(#)
 public class Theme {
-    private boolean includedInTour;
-    private double[] tagTheme;
+  private boolean includedInTour;
+  private double[] tagTheme;
 
-    public Theme(double [] tagTheme) {
-        this.tagTheme = tagTheme;
-        includedInTour = false;
-    }
+  public Theme(double[] tagTheme) {
+    this.tagTheme = tagTheme;
+    includedInTour = false;
+  }
 
-    public void toggleInclusion() {
-        this.includedInTour = !this.includedInTour;
-    }
+  public void toggleInclusion() {
+    this.includedInTour = !this.includedInTour;
+  }
 
-    public double[] getTagTheme() {
-        return tagTheme;
-    }
+  public double[] getTagTheme() {
+    return tagTheme;
+  }
 }
