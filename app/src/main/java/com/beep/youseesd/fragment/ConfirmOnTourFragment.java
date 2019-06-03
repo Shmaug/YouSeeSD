6b5b38c04e217e2d658cdfab4990a2223a5bf5a7
immmjimmy <immmjimmy@gmail.com>
2019-06-03 02:36:54 -0700

    refactored and documented fragments

--- app/src/main/java/com/beep/youseesd/fragment/ConfirmOnTourFragment.java
+++ app/src/main/java/com/beep/youseesd/fragment/ConfirmOnTourFragment.java
@@ -19,48 +19,88 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 
+/**
+ * Fragment that handles our confirmation screen before starting tour
+ */
 public class ConfirmOnTourFragment extends Fragment {
 
   private ConfirmLocationAdapter adapter;
   private RecyclerView confirmTourListView;
   private LinearLayoutManager layoutManager;
 
+  /**
+   * Lifecycle method that will inflate our fragment layout
+   *
+   * @param inflater the inflater that will be used to inflate our layout
+   * @param container the container that our layout be contained within
+   * @param savedInstanceState state of our app
+   * @return the view that was loaded
+   */
   @Nullable
   @Override
-  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
     return inflater.inflate(R.layout.fragment_confirm_tour, container, false);
   }
 
+  /**
+   * Lifecycle method that calls on the super method
+   *
+   * @param savedInstanceState used for the super call
+   */
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
   }
 
+  /**
+   * Lifecycle method that links UI elements and loads the tours
+   */
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
 
-
-    confirmTourListView = (RecyclerView) view.findViewById(R.id.list_confirm_location);
+    // link UI elements and set the layoutManager
+    confirmTourListView = view.findViewById(R.id.list_confirm_location);
     layoutManager = new LinearLayoutManager(view.getContext());
     confirmTourListView.setLayoutManager(layoutManager);
 
+    // get the current user's id and load the tour in question
     String uid = App.getUser().getUid();
     String tourId = getArguments().getString(DatabaseUtil.TOUR_ID);
     loadLocations(uid, tourId);
   }
 
+  /**
+   * Helper method that will update our UI based on the tour passed in
+   *
+   * @param t the tour that the user selected
+   */
   private void updateUI(Tour t) {
+    // use our adapter to display information on the locations in our tour
     adapter = new ConfirmLocationAdapter();
     confirmTourListView.setAdapter(adapter);
     adapter.updateData(t);
   }
 
+  /**
+   * Helper method that loads the locations within our tour by requesting data from database
+   *
+   * @param uid the id of the user
+   * @param tourId the id of the tour under the user's account
+   */
   private void loadLocations(String uid, String tourId) {
+    // get a reference to our database
     DatabaseReference tourRef = DatabaseUtil.getSingleTourDatabase(uid, tourId);
     tourRef.addListenerForSingleValueEvent(new ValueEventListener() {
+      /**
+       * Get the tour in question from our database and call on the helper method
+       *
+       * @param dataSnapshot the data that we are attempting to retrieve
+       */
       @Override
       public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+        // try to parse the data as a Tour object
         Tour t = dataSnapshot.getValue(Tour.class);
         if (t == null) {
           return;
--- app/src/main/java/com/beep/youseesd/fragment/TourListFragment.java
+++ app/src/main/java/com/beep/youseesd/fragment/TourListFragment.java
@@ -27,6 +27,9 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Fragment that lists the tours on the home page
+ */
 public class TourListFragment extends Fragment {
 
   private RecyclerView tourListView;
@@ -35,36 +38,65 @@ public class TourListFragment extends Fragment {
   private ProgressBar progressBar;
   private ImageView mEmptyImageView;
 
+  /**
+   * Lifecycle method that will inflate our fragment layout
+   *
+   * @param inflater the inflater that will be used to inflate our layout
+   * @param container the container that our layout be contained within
+   * @param savedInstanceState state of our app
+   * @return the view that was loaded
+   */
   @Nullable
   @Override
-  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
     return inflater.inflate(R.layout.fragment_home, container, false);
   }
 
+  /**
+   * Lifecycle method that handles the displays the tours onto the view
+   *
+   * @param view the view we're placing our tours on
+   * @param savedInstanceState used in the super call
+   */
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
 
-    mEmptyImageView = (ImageView) view.findViewById(R.id.home_empty_image);
-    progressBar = (ProgressBar) view.findViewById(R.id.home_progressbar);
-    tourListView = (RecyclerView) view.findViewById(R.id.list_tour);
+    // link UI elements
+    mEmptyImageView = view.findViewById(R.id.home_empty_image);
+    progressBar = view.findViewById(R.id.home_progressbar);
+    tourListView = view.findViewById(R.id.list_tour);
 
+    // if the tour is empty display a background image so it's not just an empty page
     Glide.with(getActivity())
         .load(getResources().getDrawable(R.drawable.tour_empty))
         .apply(RequestOptions.circleCropTransform())
         .into(mEmptyImageView);
 
+    // link the adapter and layoutManager and load the tours using the user's id
     adapter = new HomeTourAdapter((HomeActivity) getActivity(), new ArrayList<>());
     layoutManager = new LinearLayoutManager(view.getContext());
     tourListView.setLayoutManager(layoutManager);
     loadTours(App.getUser().getUid());
   }
 
+  /**
+   * Helper method that loads the tours under a user's id from database
+   *
+   * @param uid the user's id
+   */
   private void loadTours(String uid) {
     DatabaseUtil.getTourDatabase(uid).addValueEventListener(new ValueEventListener() {
+      /**
+       * Get the tours underneath a user and store them into a list for use
+       *
+       * @param dataSnapshot all of the tours under a user's account
+       */
       @Override
       public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
         List<Tour> tours = new ArrayList<>();
+        // go through the children and parse each element as a Tour object
         for (DataSnapshot ds : dataSnapshot.getChildren()) {
           String dsid = ds.getKey();
           Tour t = ds.getValue(Tour.class);
@@ -72,18 +104,25 @@ public class TourListFragment extends Fragment {
           tours.add(t);
         }
 
+        // using the retrieved data, pass through the adapter to force an update
         adapter.updateTours(tours);
         tourListView.setAdapter(adapter);
         progressBar.setVisibility(View.GONE);
         mEmptyImageView.setVisibility(tours.isEmpty() ? View.VISIBLE : View.GONE);
       }
 
+      /**
+       * Handles the error when our tours weren't loaded properly
+       *
+       * @param databaseError object containing information regarding the error that occurred
+       */
       @Override
       public void onCancelled(@NonNull DatabaseError databaseError) {
         WLog.e(databaseError.getMessage());
         progressBar.setVisibility(View.GONE);
         mEmptyImageView.setVisibility(View.VISIBLE);
-        Snackbar.make(getView(), "Something went wrong. Failed to load tours", Snackbar.LENGTH_LONG).show();
+        Snackbar.make(getView(), "Something went wrong. Failed to load tours", Snackbar.LENGTH_LONG)
+            .show();
       }
     });
   }
