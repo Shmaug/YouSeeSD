9c8ab907dc648d963a57d386b17779f402e681b9
immmjimmy <immmjimmy@gmail.com>
2019-06-03 02:24:48 -0700

    commented application

--- app/src/main/java/com/beep/youseesd/application/App.java
+++ app/src/main/java/com/beep/youseesd/application/App.java
@@ -5,10 +5,16 @@ import com.beep.youseesd.util.WLog;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+/**
+ * Class that handles the creation of our app
+ */
 public class App extends Application {
 
   private static App singleton = null;
 
+  /**
+   * Lifecycle method that will only create one instance of our app using the singleton pattern
+   */
   @Override
   public void onCreate() {
     super.onCreate();
@@ -16,10 +22,20 @@ public class App extends Application {
     WLog.i("Our app just started!");
   }
 
+  /**
+   * Getter for our singleton if necessary
+   *
+   * @return our app singleton
+   */
   public static App getInstance() {
     return singleton;
   }
 
+  /**
+   * Getter for the current user in firebase
+   *
+   * @return the current user as a FirebaseUser object
+   */
   public static FirebaseUser getUser() {
     return FirebaseAuth.getInstance().getCurrentUser();
   }
