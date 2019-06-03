60fcec6ddc1e7e34926328a211f45f7325c0b70c
immmjimmy <immmjimmy@gmail.com>
2019-06-03 02:40:52 -0700

    refactored and commented handler

--- app/src/main/java/com/beep/youseesd/handler/AuthHandler.java
+++ app/src/main/java/com/beep/youseesd/handler/AuthHandler.java
@@ -1,23 +1,25 @@
 package com.beep.youseesd.handler;
 
 import android.app.Activity;
-import androidx.annotation.NonNull;
 import com.beep.youseesd.util.WLog;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+/**
+ * Handles the authentication of a user in Firebase
+ */
 public class AuthHandler {
 
+    /**
+     * Attempt to sign in anonymously since we don't really store sensitive information
+     *
+     * @param act the activity we're performing the authentication upon
+     * @param listener the listener to add to our firebase if logging in is successful
+     */
     public static void signinAnonymously(Activity act, OnCompleteListener<AuthResult> listener) {
-        FirebaseAuth.getInstance().signInAnonymously().addOnFailureListener(act, new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                WLog.i("user login failed");
-            }
-        });
-
+        FirebaseAuth.getInstance().signInAnonymously().addOnFailureListener(act,
+            e -> WLog.i("user login failed"));
         FirebaseAuth.getInstance().signInAnonymously().addOnCompleteListener(act, listener);
     }
 }
