66bfbaf1df0b5857987bb74f666bd03363677676
Yong Do <magicdo7@gmail.com>
2019-06-04 02:40:57 -0700

    adds comments

--- app/src/main/java/com/beep/youseesd/model/Weather.java
+++ app/src/main/java/com/beep/youseesd/model/Weather.java
@@ -1,25 +1,40 @@
 package com.beep.youseesd.model;
 
+/**
+ * Holds the info about the weather
+ */
 public class Weather {
 
   private String mUnit;
   private String mWeather;
   private int mTemperature;
 
+  /**
+   * A constructor to get the current weather info
+   */
   public Weather(String unit, String weather, int temperature) {
     this.mUnit = unit;
     this.mWeather = weather;
     this.mTemperature = temperature;
   }
 
+  /**
+   * A getter for temperature
+   */
   public int getTemperature() {
     return mTemperature;
   }
 
+  /**
+   * A getter for unit
+   */
   public String getUnit() {
     return mUnit;
   }
 
+  /**
+   * A getter for weather
+   */
   public String getWeather() {
     return mWeather;
   }
